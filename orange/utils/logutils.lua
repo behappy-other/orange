b822bda65e538059d6972fe025ab04c64d54c283
xiaowu <943915349@qq.com>
2023-08-24 09:47:08 +0800

    测试

--- orange/utils/logutils.lua
+++ orange/utils/logutils.lua
@@ -1,7 +1,7 @@
 local _M = {}
 
 function _M.log(level, ...)
-    if ngx and ngx.req then
+    if ngx ~= nil and ngx.req ~= nil then
         local headers = ngx.req.get_headers()
         if headers ~= nil and next(headers) ~= nil then
             return require("orange.utils.logutils").log(level, '[',os.date("%Y-%m-%d %H:%M:%S") ,'] ~ gateway_orange_service ~ ', (headers["trace-id"] or "-"), ' ~ ', '-', ' ~ ', ngx.var.remote_addr, ' ~ ', ...)
