19c091116c66c2ce7ef083df6e9afe4de66068d9
xiaowu <943915349@qq.com>
2023-08-24 10:30:19 +0800

    测试

--- orange/utils/logutils.lua
+++ orange/utils/logutils.lua
@@ -1,7 +1,7 @@
 local _M = {}
 
 function _M.log(level, ...)
-    ngx.log(level, ' ~ [',os.date("%Y-%m-%d %H:%M:%S") ,'] ~ gateway_orange_service ~ ', '-',' ~ ', '-', ' ~ ', '-', ' ~ ', ...)
+    ngx.log(level, '~ ',os.date("%Y-%m-%d %H:%M:%S.%f") ,' ~ gateway_orange_service ~ ', '-',' ~ ', '-', ' ~ ', '-', ' ~ ', ...)
 end
 
 return _M
